719d881b5fe2a9d5bd17998a16a044a0218dbe03
AsakuraFuuko <asakurafuko@gmail.com>
2016-11-24 11:09:05 +0800

    增加简体中文翻译，修复文字对齐，修复和marktioneer的冲突

---
+++ marketshowlevel/src/marketshowlevel_sc/marketshowlevel.lua
@@ -0,0 +1,319 @@
+-- dofile("../data/addon_d/marketshowlevel/marketshowlevel.lua");
+
+CHAT_SYSTEM("MARKET SHOW LEVEL SC v1.0.4 loaded!");
+
+local itemColor = {
+	[0] = "FFFFFF",    -- Normal
+	[1] = "108CFF",    -- 0.75 over
+	[2] = "9F30FF",    -- 0.85 over
+	[3] = "FF4F00",    -- 0.95 over
+};
+
+local propList = {};
+propList.MHP           = {name = "ＨＰ";max = 2283;};
+propList.RHP           = {name = "HP回";max = 56;};
+propList.MSP           = {name = "ＳＰ";max = 450;};
+propList.RSP           = {name = "SP回";max = 56;};
+propList.PATK          = {name = "物攻";max = 126;};
+propList.ADD_MATK      = {name = "魔攻";max = 126;};
+propList.ADD_DEF       = {name = "物防";max = 110;};
+propList.ADD_MDEF      = {name = "魔防";max = 110;};
+propList.ADD_MHR       = {name = "増幅";max = 126;};
+propList.CRTATK        = {name = "爆攻";max = 189;};
+propList.CRTHR         = {name = "爆发";max = 14;};
+propList.CRTDR         = {name = "爆抵";max = 14;};
+propList.BLK           = {name = "格挡";max = 14;};
+propList.ADD_HR        = {name = "命中";max = 14;};
+propList.ADD_DR        = {name = "回避";max = 14;};
+propList.ADD_FIRE      = {name = "火攻";max = 99;};
+propList.ADD_ICE       = {name = "冰攻";max = 99;};
+propList.ADD_POISON    = {name = "毒攻";max = 99;};
+propList.ADD_LIGHTNING = {name = "雷攻";max = 99;};
+propList.ADD_EARTH     = {name = "地攻";max = 99;};
+propList.ADD_SOUL      = {name = "念攻";max = 99;};
+propList.ADD_HOLY      = {name = "圣攻";max = 99;};
+propList.ADD_DARK      = {name = "暗攻";max = 99;};
+propList.RES_FIRE      = {name = "火防";max = 84;};
+propList.RES_ICE       = {name = "冰防";max = 84;};
+propList.RES_POISON    = {name = "毒防";max = 84;};
+propList.RES_LIGHTNING = {name = "雷防";max = 84;};
+propList.RES_EARTH     = {name = "地防";max = 84;};
+propList.RES_SOUL      = {name = "念防";max = 84;};
+propList.RES_HOLY      = {name = "圣防";max = 84;};
+propList.RES_DARK      = {name = "暗防";max = 84;};
+propList.MSPD          = {name = "移动";max = 1;};
+propList.SR            = {name = "AOE攻";max = 1;};
+propList.SDR           = {name = "AOE防";max = 4;};
+
+local function SetupHook(newFunction, hookedFunctionName)
+	local storeOldFunc = hookedFunctionName .. "_OLD_MARKET";
+	if _G[storeOldFunc] == nil then
+		_G[storeOldFunc] = _G[hookedFunctionName];
+		_G[hookedFunctionName] = newFunction;
+	else
+		_G[hookedFunctionName] = newFunction;
+	end
+end
+
+local isLoaded = false;
+
+function MARKETSHOWLEVEL_ON_INIT(addon, frame)
+	if not isLoaded then
+		SetupHook(ON_MARKET_ITEM_LIST_HOOKED, "ON_MARKET_ITEM_LIST");
+		isLoaded = true;
+	end
+end
+
+function GetItemValueColor(propname,value, max)
+	local index = 0;
+
+	if propname == "MSPD" or propname == "SR" or propname == "SDR" then
+		index = 0
+	else
+		if value > (max * 0.95) then
+			index = 3
+		elseif value > (max * 0.85) then
+			index = 2
+		elseif value > (max * 0.75) then
+			index = 1
+		end
+	end
+
+	return itemColor[index]
+end
+
+
+function GetGemInfo(itemObj)
+	local gemInfo = "";
+	local fn = GET_FULL_NAME_OLD or GET_FULL_NAME;
+
+	local socketId;
+	local rstLevel;
+	local gemName;
+	local exp;
+	-- local space= "";
+	local color="";
+
+	for i = 0, 4 do
+
+		socketId = itemObj["Socket_Equip_" .. i];
+		rstLevel = itemObj["Socket_JamLv_" .. i];
+		exp = itemObj["SocketItemExp_" .. i];
+
+		if socketId > 0 then
+			if #gemInfo > 0 then
+				gemInfo = gemInfo..",";
+				-- space = space .. "  ";
+			end
+			-- if i==3 then
+			-- 	space = space .. " ";
+			-- end
+
+			local obj = GetClassByType("Item", socketId);
+			gemName = fn(obj);
+			local gemLevel = 0;
+
+			if exp >= 27014700 then
+				gemLevel = 10;
+			elseif exp >= 5414700 then
+				gemLevel = 9;
+			elseif exp >= 1094700 then
+				gemLevel = 8;
+			elseif exp >= 230700 then
+				gemLevel = 7;
+			elseif exp >= 57900 then
+				gemLevel = 6;
+			elseif exp >= 14700 then
+				gemLevel = 5;
+			elseif exp >= 3900 then
+				gemLevel = 4;
+			elseif exp >= 1200 then
+				gemLevel = 3;
+			elseif exp >= 300 then
+				gemLevel = 2;
+			else
+				gemLevel = 1;
+			end
+
+			if gemLevel <= rstLevel then
+				gemInfo = gemInfo .. "{#FF7F50}{ol}Lv" .. gemLevel .. ":" .. gemName .. "{/}{/}";
+			else
+				gemInfo = gemInfo .. "{#FFFFFF}{ol}Lv" .. gemLevel .. ":" .. gemName .. "{/}{/}";
+			end
+
+			-- space = space .. "                 ";
+
+		end
+	end
+
+	if #gemInfo > 0 then
+		gemInfo = "{nl}" .. gemInfo;
+	end
+
+	return gemInfo;
+
+end
+
+function GetHatProp(itemObj)
+	local prop = "";
+	-- local space= "";
+	for i = 1 , 3 do
+		local propName = "";
+		local propValue = 0;
+		local propNameStr = "HatPropName_"..i;
+		local propValueStr = "HatPropValue_"..i;
+		if itemObj[propValueStr] ~= 0 and itemObj[propNameStr] ~= "None" then
+			if #prop > 0 then
+				prop = prop..",";
+				-- space = space .. " ";
+			end
+
+			propName = itemObj[propNameStr];
+			propValue = itemObj[propValueStr];
+
+			propValueColored = GetItemValueColor(propName, propValue, propList[propName].max);
+
+			prop = prop .. string.format("%s:{#%s}{ol}%4d{/}{/}", propList[propName].name, propValueColored, propValue);
+			-- space = space .. "         ";
+		end
+	end
+
+	if #prop > 0 then
+		prop = "{nl}" .. prop;
+	end
+
+	return prop;
+
+end
+
+function ON_MARKET_ITEM_LIST_HOOKED(frame, msg, argStr, argNum)
+	if frame:IsVisible() == 0 then
+		return;
+	end
+
+	local itemlist = GET_CHILD(frame, "itemlist", "ui::CDetailListBox");
+	itemlist:RemoveAllChild();
+	local mySession = session.GetMySession();
+	local cid = mySession:GetCID();
+
+	local count = session.market.GetItemCount();
+	for i = 0 , count - 1 do
+		local marketItem = session.market.GetItemByIndex(i);
+		local itemObj = GetIES(marketItem:GetObject());
+
+-- add code start
+		local itemLevel = GET_ITEM_LEVEL(itemObj);
+		local itemGroup = itemObj.GroupName;
+-- add code end
+
+		local refreshScp = itemObj.RefreshScp;
+		if refreshScp ~= "None" then
+			refreshScp = _G[refreshScp];
+			refreshScp(itemObj);
+		end	
+
+		local ctrlSet = INSERT_CONTROLSET_DETAIL_LIST(itemlist, i, 0, "market_item_detail");
+		ctrlSet = tolua.cast(ctrlSet, "ui::CControlSet");
+		ctrlSet:EnableHitTestSet(1);
+		ctrlSet:SetUserValue("DETAIL_ROW", i);
+
+		SET_ITEM_TOOLTIP_ALL_TYPE(ctrlSet, marketItem, itemObj.ClassName, "market", marketItem.itemType, marketItem:GetMarketGuid());
+
+		local pic = GET_CHILD(ctrlSet, "pic", "ui::CPicture");
+		pic:SetImage(itemObj.Icon);
+
+		local name = ctrlSet:GetChild("name");
+
+-- add code start
+
+		if itemGroup == "Weapon" or itemGroup == "SubWeapon" then
+			local gemInfo = GetGemInfo(itemObj);
+			name:SetTextByKey("value", GET_FULL_NAME(itemObj) .. gemInfo);
+		elseif itemGroup == "Armor" then
+			local gemInfo = GetGemInfo(itemObj);
+			local prop = GetHatProp(itemObj);
+			name:SetTextByKey("value", GET_FULL_NAME(itemObj) .. prop .. gemInfo);
+		elseif itemGroup == "Gem" or itemGroup == "Card" then
+			name:SetTextByKey("value", "Lv".. itemLevel .. ":" .. GET_FULL_NAME(itemObj));
+		elseif (itemObj.ClassName == "Scroll_SkillItem") then
+			local skillClass = GetClassByType("Skill", itemObj.SkillType);
+			name:SetTextByKey("value", "Lv".. itemObj.SkillLevel .. " " .. skillClass.Name .. ":" .. GET_FULL_NAME(itemObj));
+		else
+			name:SetTextByKey("value", GET_FULL_NAME(itemObj));
+		end
+
+		name:SetTextAlign("left", "center");
+
+-- add code end
+
+		local count = ctrlSet:GetChild("count");
+		count:SetTextByKey("value", marketItem.count);
+		
+		local level = ctrlSet:GetChild("level");
+		level:SetTextByKey("value", itemObj.UseLv);
+
+		local price = ctrlSet:GetChild("price");
+		price:SetTextByKey("value", GetCommaedText(marketItem.sellPrice));
+		price:SetUserValue("Price", marketItem.sellPrice);
+		if cid == marketItem:GetSellerCID() then
+			local button_1 = ctrlSet:GetChild("button_1");
+			button_1:SetEnable(0);
+
+			local btnmargin = 639
+			if USE_MARKET_REPORT == 1 then
+				local button_report = ctrlSet:GetChild("button_report");
+				button_report:SetEnable(0);
+				btnmargin = 720
+			end
+
+			local btn = ctrlSet:CreateControl("button", "DETAIL_ITEM_" .. i, btnmargin, 8, 100, 50);
+			btn = tolua.cast(btn, "ui::CButton");
+			btn:ShowWindow(1);
+			btn:SetText("{@st41b}" .. ClMsg("Cancel"));
+			btn:SetTextAlign("center", "center");
+
+			if notUseAnim ~= true then
+				btn:SetAnimation("MouseOnAnim", "btn_mouseover");
+				btn:SetAnimation("MouseOffAnim", "btn_mouseoff");
+			end
+			btn:UseOrifaceRectTextpack(true)
+			btn:SetEventScript(ui.LBUTTONUP, "CANCEL_MARKET_ITEM");
+			btn:SetEventScriptArgString(ui.LBUTTONUP,marketItem:GetMarketGuid());
+			btn:SetSkinName("test_pvp_btn");
+			local totalPrice = ctrlSet:GetChild("totalPrice");
+			totalPrice:SetTextByKey("value", 0);
+		else
+			local btnmargin = 639
+			if USE_MARKET_REPORT == 1 then
+				btnmargin = 560
+			end
+			local numUpDown = ctrlSet:CreateControl("numupdown", "DETAIL_ITEM_" .. i, btnmargin, 20, 100, 30);
+			numUpDown = tolua.cast(numUpDown, "ui::CNumUpDown");
+			numUpDown:SetFontName("white_18_ol");
+			numUpDown:MakeButtons("btn_numdown", "btn_numup", "editbox");
+			numUpDown:ShowWindow(1);
+			numUpDown:SetMaxValue(marketItem.count);
+			numUpDown:SetMinValue(1);
+			numUpDown:SetNumChangeScp("MARKET_CHANGE_COUNT");
+			numUpDown:SetClickSound('button_click_chat');
+			numUpDown:SetNumberValue(1)
+
+			local totalPrice = ctrlSet:GetChild("totalPrice");
+				totalPrice:SetTextByKey("value", GetCommaedText(marketItem.sellPrice));
+				totalPrice:SetUserValue("Price", marketItem.sellPrice);
+		end		
+	end
+
+	itemlist:RealignItems();
+	GBOX_AUTO_ALIGN(itemlist, 10, 0, 0, false, true);
+
+	local maxPage = math.ceil(session.market.GetTotalCount() / MARKET_ITEM_PER_PAGE);
+	local curPage = session.market.GetCurPage();
+	local pagecontrol = GET_CHILD(frame, 'pagecontrol', 'ui::CPageController')
+	pagecontrol:SetMaxPage(maxPage);
+	pagecontrol:SetCurPage(curPage);
+
+	if nil ~= argNum and  argNum == 1 then
+		MARGET_FIND_PAGE(frame, session.market.GetCurPage());
+	end
+end
